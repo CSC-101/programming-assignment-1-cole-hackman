beea731533cc0e01e3f07eba4dd7b67ea5efb57d
cole-hackman <hackman@calpoly.edu>
2024-10-21 16:47:30 -0700

    Implemented functions and 2 corresponding test cases for each to validate functionality and edge cases
    - Implemented vowel_count(inputStr) to count the number of vowels in a string
    - Implemented short_lists(inputList) to filter for nested lists of length 2
    - Implemented ascending_pairs(inputList) to reorder pairs of integers in ascending order
    - Implemented add_prices(price1, price2) to sum two Price objects
    - Implemented rectangle_area(rectangle) to compute the area of a rectangle
    - Implemented books_by_author(author, book_catalog) to return books by a specific author
    - Added circle_bound(rectangle) to return a bounding circle for a rectangle
    - Implemented below_pay_average(employees)to list employees earning below average pay

--- hw1.py
+++ hw1.py
@@ -1,28 +1,90 @@
+import unittest
 import data
+import math
+
+from data import Employee, Price, Rectangle, Circle, Book, Employee, Point
 
 # Write your functions for each part in the space below.
 
 # Part 1
+# This function accepts a string as input, and returns the number of vowels that appear in the string
+def vowel_count(inputStr: str) -> int:
+    vowelNum= 0
+    for char in inputStr.lower():
+        if char in ("a","e","i","o","u"):
+            vowelNum += 1
+    return vowelNum
 
 
 # Part 2
+# This function accepts a list of nested lists, and returns the nested lists of length 2
+def short_lists(inputList: list[list[int]])-> list:
+    newList = []
+    for sublist in inputList:
+        if len(sublist) == 2:
+            newList.append(sublist)
+    return newList
 
 
 # Part 3
+# This function accepts a list of nested lists, and returns the nested lists where pairs of two integers are in ascending order
+def ascending_pairs(inputList: list[list[int]]):
+    newList = []
+    for sublist in inputList:
+        if len(sublist) == 2:
+            if sublist[0] > sublist[1]:
+                sublist[0], sublist[1] = sublist[1], sublist[0]
+            newList.append(sublist)
+        else:
+            newList.append(sublist)
+    return newList
 
 
 # Part 4
+# This function accepts two Price objects, and returns a new Price object that is the sum of the two
+def add_prices(price1: Price, price2: Price) -> Price:
+    sum_dollars = price1.dollars + price2.dollars
+    sum_cents = price1.cents + price2.cents
+    if (sum_cents) >= 100:
+        sum_dollars += (sum_cents // 100)
+        sum_cents = sum_cents % 100
+    return Price(sum_dollars, sum_cents)
 
 
 # Part 5
-
+# This function accepts a Rectangle object, and returns the area of the rectangle
+def rectangle_area(rectangle: Rectangle) -> float:
+    width = abs(rectangle.bottom_right.x - rectangle.top_left.x)
+    height = abs(rectangle.top_left.y - rectangle.bottom_right.y)
+    return width * height
 
 # Part 6
+# This function accepts an author's name and a list of Book objects, and returns a list of books written by that author
+def books_by_author(author: str, book_catalog: list[Book]) -> list[Book]:
+    same_author = [book for book in book_catalog if author in book.authors]
+    return same_author
 
 
 # Part 7
+# This function accepts a Rectangle object, and returns a Circle object that bounds the rectangle
+def circle_bound(rectangle: Rectangle) -> Circle:
+    circle_center_x = (rectangle.top_left.x + rectangle.bottom_right.x) / 2
+    circle_center_y = (rectangle.top_left.y + rectangle.bottom_right.y) / 2
+    center = Point(circle_center_x, circle_center_y)
+
+    radius = math.sqrt((circle_center_x - rectangle.bottom_right.x)**2 + (circle_center_y - rectangle.bottom_right.y)**2)
+    return Circle(center, radius)
 
 
 # Part 8
+# This function accepts a list of Employee objects, and returns a list of employees who earn under the average pay rate
+def below_pay_average(employees: list[Employee]) -> list:
+    if len(employees) == 0:
+        return []
+
+    pay_average = sum(employee.pay_rate for employee in employees) / len(employees)
+    below_pay_employees = [employee.name for employee in employees if employee.pay_rate < pay_average]
+    return below_pay_employees
+
 
 
--- hw1_test.py
+++ hw1_test.py
@@ -1,37 +1,113 @@
-import data
-import hw1
 import unittest
-
-
+import math
+from hw1 import vowel_count, short_lists, ascending_pairs, add_prices, rectangle_area, books_by_author, circle_bound, below_pay_average
+from data import Price, Point, Rectangle, Circle, Book, Employee
 # Write your test cases for each part below.
 
 class TestCases(unittest.TestCase):
     # Part 1
+    def test_vowel_count_1(self):
+        result = vowel_count("hello")
+        expected = 2
+        self.assertEqual(result, expected)
+
+    def test_vowel_count_2(self):
+        result = vowel_count("by")
+        expected = 0
+        self.assertEqual(result, expected)
 
 
     # Part 2
+    def test_short_lists_1(self):
+        result = short_lists([[3, 4], [1, 2, 9, 0], [1], [5, 6]])
+        expected = [[3, 4], [5, 6]]
+        self.assertEqual(result, expected)
+
+    def test_short_lists_2(self):
+        result = short_lists([[3, 4, 8], [1, 2, 9, 0], [1], [7, 5, 6]])
+        expected = []
+        self.assertEqual(result, expected)
 
 
     # Part 3
+    def test_ascending_pairs_1(self):
+        result = ascending_pairs([[3, 3], [6, 4], [5, 1]])
+        expected = [[3, 3], [4, 6], [1, 5]]
+        self.assertEqual(result, expected)
+
+    def test_ascending_pairs_2(self):
+        result = ascending_pairs([[6, 3], [9, 4], [1, 1]])
+        expected = [[3, 6], [4, 9], [1, 1]]
+        self.assertEqual(result, expected)
 
 
     # Part 4
+    def test_add_prices_1(self):
+        result = add_prices(Price(1, 75), Price(2, 50))
+        expected = Price(4, 25)
+        self.assertEqual(result.dollars, expected.dollars)
+        self.assertEqual(result.cents, expected.cents)
+
+    def test_add_prices_2(self):
+        result = add_prices(Price(1, 2), Price(2, 99))
+        expected = Price(4, 1)
+        self.assertEqual(result.dollars, expected.dollars)
+        self.assertEqual(result.cents, expected.cents)
 
 
     # Part 5
+    def test_rectangle_area_1(self):
+        result = rectangle_area(Rectangle(Point(1,5), Point(6,1)))
+        expected = 20
+        self.assertEqual(result, expected)
+
+    def test_rectangle_area_2(self):
+        result = rectangle_area(Rectangle(Point(0,10), Point(10,0)))
+        expected = 100
+        self.assertEqual(result, expected)
 
 
     # Part 6
+    def test_books_by_author_1(self):
+        b1 = Book("X Author", ["A Book"])
+        b2 = Book("Y Author", ["B Book"])
+        b3 = Book("X Author", ["C Book"])
+        result = books_by_author("X Author", [b1, b2, b3])
+        expected = [b1, b3]
+        self.assertEqual(result, expected)
 
+    def test_books_by_author_12(self):
+        b1 = Book("X Author", ["A Book"])
+        b2 = Book("Y Author", ["B Book"])
+        b3 = Book("Z Author", ["C Book"])
+        result = books_by_author("Z Author", [b1, b2, b3])
+        expected = [b3]
+        self.assertEqual(result, expected)
 
     # Part 7
+    def test_circle_bound_1(self):
+        result = circle_bound(Rectangle(Point(0.0,4.0), Point(4.0,0.0)))
+        expected = Circle(Point(2.0,2.0), 2.8284271247461903)
+        self.assertEqual(result.center.x, expected.center.x)
+        self.assertEqual(result.center.y, expected.center.y)
+        self.assertAlmostEqual(result.radius, expected.radius)
 
-
+    def test_circle_bound_2(self):
+        result = circle_bound(Rectangle(Point(10.0,8.0), Point(15.0,4.0)))
+        expected = Circle(Point(12.5,6.0), 3.2015621187)
+        self.assertEqual(result.center.x, expected.center.x)
+        self.assertEqual(result.center.y, expected.center.y)
+        self.assertAlmostEqual(result.radius, expected.radius)
     # Part 8
+    def test_below_pay_average_1(self):
+        result = below_pay_average([Employee("Bob", 10), Employee("Jake", 20), Employee("Charlie", 30)])
+        expected = ["Bob"]
+        self.assertEqual(result, expected)
 
-
-
-
+    def test_below_pay_average_1(self):
+        result = below_pay_average([Employee("Bob", 500), Employee("Jake", 600), Employee("Charlie", 700)])
+        expected = ["Bob"]
+        self.assertEqual(result, expected)
 
 if __name__ == '__main__':
     unittest.main()
